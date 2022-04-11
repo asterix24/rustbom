ee017c4e1fa26cb2e112cb4cf6a663fdf7fe351e
Daniele Basile <asterix24@gmail.com>
2022-04-11 23:06:49 +0200

    use axum instead tide

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,74 @@
-use tide::{http::mime, Body, Redirect, Request, Response, Server, StatusCode};
-use tide::prelude::*;
+use askama::Template;
+use axum::routing::post;
+use axum::{extract::Form, response::Html, routing::get, Json, Router};
+use serde::Deserialize;
+use serde_json::json;
+use std::net::SocketAddr;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod lib;
-use lib::load::{Load, XlsxLoader};
 use lib::item::Item;
+use lib::load::{Load, XlsxLoader};
 
-#[derive(Debug, Deserialize)]
-struct Due {
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "example_form=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new()
+        .route("/", get(show_form))
+        .route("/data/", post(merge_view_post));
+
+    // run it with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn show_form() -> Html<&'static str> {
+    Html(std::include_str!("../templates/index.html"))
+}
+
+async fn merge_view_post() -> Json<Item> {
+    let item = Item::new();
+    Json(item)
+}
+
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+struct Input {
     name: String,
-    reference: String,
+    email: String,
 }
 
-#[async_std::main]
-async fn main() -> tide::Result<()> {
-    tide::log::start();
-    let mut app = tide::new();
-    app.at("/").get(uno_post);
-    app.listen("127.0.0.1:8080").await?;
-    Ok(())
+async fn preview_post(Form(input): Form<Input>) {
+    dbg!(&input);
 }
 
-async fn uno_post(mut req: Request<()>) -> tide::Result {
-    //let uno = req.body_form().await?;
-    let mut ld: XlsxLoader = Load::new("/Users/asterix/src/github/mergebom-web/boms/test0.xlsx");
+// async fn preview_post(mut req: Request<()>) -> tide::Result {
+//     //let uno = req.body_json().await?;
+//     //ide::log::info!("{:?}", uno);
+//     let mut ld: XlsxLoader = Load::new("/Users/asterix/src/github/mergebom-web/boms/test0.xlsx");
 
-    ld.read();
-    let d = ld.raw_data();
-    let mut items: Vec<Item> = Vec::new();
-    for row in  d {
-        let mut item = Item::new();
-        item.push(row, ld.map_data());
-        items.push(item);
-    }
+//     ld.read();
+//     let d = ld.raw_data();
+//     let mut items: Vec<Item> = Vec::new();
 
-    let ret = serde_json::to_string(&items).unwrap();
-    tide::log::info!("{}", ret);
+//     for row in d {
+//         let mut item = Item::new();
+//         item.push(row, ld.map_data());
+//         items.push(item);
+//     }
 
-    Ok(Response::builder(StatusCode::Ok).body(Body::from_json(&ret)?).build())
-}
+//     match Body::from_json(&items) {
+//         Ok(body) => Ok(body.into()),
+//         Err(_) => Ok(Response::new(StatusCode::NotFound)),
+//     }
+// }
