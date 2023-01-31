2b8067ab4a6f6316d72e76ae0e56faa9ca3a725b
Daniele Basile <asterix24@gmail.com>
2023-01-31 21:38:33 +0100

    Clean up

--- src/tests/merge.rs
+++
@@ -1,8 +0,0 @@
-use mergebom_web::bom::{merge_key_list, Bom, ItemsTable};
-
-#[test]
-fn merge() {
-    let bom = Bom::loader("data/bom_test1.csv", &["comment".to_string()]);
-    let data = bom.merge().odered_vector_table();
-    println!("{:?}", data);
-}
