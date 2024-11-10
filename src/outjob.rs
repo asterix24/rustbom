a044b0696aef8eca96c974a59d6b34fdf1379b17
Daniele Basile <asterix24@gmail.com>
2024-11-11 00:15:07 +0100

    Update xlsxwrite to compile with new version of compiler

--- src/outjob.rs
+++ src/outjob.rs
@@ -1,7 +1,8 @@
 use std::path::Path;
 
 use super::bom::ItemsTable;
-use xlsxwriter::*;
+use xlsxwriter::prelude::{FormatAlignment, FormatBorder, FormatColor};
+use xlsxwriter::{Format, Workbook};
 
 pub struct OutJobXlsx {
     wk: Workbook,
@@ -10,37 +11,34 @@ pub struct OutJobXlsx {
 
 impl OutJobXlsx {
     pub fn new<P: AsRef<Path>>(path: P) -> OutJobXlsx {
-        OutJobXlsx {
-            wk: Workbook::new(path.as_ref().to_str().unwrap()),
-            curr_row: 0,
-        }
+        let wk = match Workbook::new(format!("{}.xlsx", path.as_ref().to_str().unwrap()).as_str()) {
+            Ok(wk) => wk,
+            _ => panic!("Unable to add sheet to open wk"),
+        };
+
+        OutJobXlsx { wk, curr_row: 0 }
     }
     pub fn write(mut self, data: &ItemsTable) {
-        let fmt_defalt = self
-            .wk
-            .add_format()
-            .set_text_wrap()
-            .set_font_size(10.0)
-            .set_text_wrap();
-        let fmt_header = self
-            .wk
-            .add_format()
-            .set_bg_color(FormatColor::Cyan)
-            .set_bold()
-            .set_font_size(12.0);
-        let fmt_category = self
-            .wk
-            .add_format()
-            .set_bg_color(FormatColor::Yellow)
-            .set_bold()
-            .set_border(FormatBorder::Thin)
-            .set_align(FormatAlignment::CenterAcross);
-        let fmt_qty = self
-            .wk
-            .add_format()
-            .set_bg_color(FormatColor::Lime)
-            .set_bold()
-            .set_font_size(12.0);
+        let mut fmt_default = Format::new();
+        fmt_default.set_text_wrap();
+        fmt_default.set_font_size(10.0);
+        fmt_default.set_text_wrap();
+
+        let mut fmt_header = Format::new();
+        fmt_header.set_bg_color(FormatColor::Cyan);
+        fmt_header.set_bold();
+        fmt_header.set_font_size(12.0);
+
+        let mut fmt_category = Format::new();
+        fmt_category.set_bg_color(FormatColor::Yellow);
+        fmt_category.set_bold();
+        fmt_category.set_border(FormatBorder::Thin);
+        fmt_category.set_align(FormatAlignment::CenterAcross);
+
+        let mut fmt_qty = Format::new();
+        fmt_qty.set_bg_color(FormatColor::Lime);
+        fmt_qty.set_bold();
+        fmt_qty.set_font_size(12.0);
 
         let mut sheet = match self.wk.add_worksheet(None) {
             Ok(wk) => wk,
@@ -49,7 +47,7 @@ impl OutJobXlsx {
 
         for (column, hdr) in (0_u16..).zip(data.headers.iter()) {
             sheet
-                .write_string(self.curr_row, column as u16, hdr, Some(&fmt_header))
+                .write_string(self.curr_row, column, hdr, Some(&fmt_header))
                 .unwrap();
         }
         self.curr_row += 1;
@@ -72,7 +70,7 @@ impl OutJobXlsx {
             // Write all fields
             for (n, d) in i.fields.iter().enumerate() {
                 //debug!("merged {}, np {}", i.is_merged, i.is_np);
-                let mut fmt = Some(&fmt_defalt);
+                let mut fmt = Some(&fmt_default);
                 if n == 0 {
                     fmt = Some(&fmt_qty);
                 }
